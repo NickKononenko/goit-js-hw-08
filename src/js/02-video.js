538eea6d0c5b90b328fdb7a0c76c5edd58f050d8
NickKononenko <kononenko0812@gmail.com>
2023-08-11 14:04:28 +0300

    2nd task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+
+const currentTime = JSON.parse(
+  localStorage.getItem('videoplayer-current-time')
+);
+
+console.dir(iframe);
+
+const player = new Player(iframe);
+
+const onPlay = throttle(data => {
+  return localStorage.setItem(
+    'videoplayer-current-time',
+    JSON.stringify(data.seconds)
+  );
+}, 1000);
+
+player.on('timeupdate', onPlay);
+
+player.setCurrentTime(currentTime);
