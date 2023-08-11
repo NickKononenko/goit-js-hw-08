8a1de63d38bf50680e0d734c1fea3a017e71b3f5
NickKononenko <kononenko0812@gmail.com>
2023-08-11 19:02:54 +0300

    task 3 done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,35 +1,27 @@
 import throttle from 'lodash.throttle';
-
 const form = document.querySelector('.feedback-form');
-const submitBtn = document.querySelector('button');
-
-const userData = {
-  email: '',
-  message: '',
-};
+let currentData;
 const savedUserData = JSON.parse(localStorage.getItem('feedback-form-state'));
 
-window.addEventListener('load', onLoad);
 form.addEventListener('input', throttle(onInput, 500));
 form.addEventListener('submit', onSubmit);
 
-function onLoad() {
-  if (savedUserData) {
-    form.email.value = savedUserData.email;
-    form.message.value = savedUserData.message;
-  }
+if (savedUserData) {
+  form.email.value = savedUserData.email;
+  form.message.value = savedUserData.message;
 }
 
 function onInput() {
-  userData.email = form.email.value;
-  userData.message = form.message.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+  currentData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(currentData));
 }
 
 function onSubmit(event) {
-  if (event.target.nodeName === 'BUTTON') {
-    event.preventDefault();
-    localStorage.removeItem('feedback-form-state');
-    console.log(savedUserData);
-  }
+  event.preventDefault();
+  console.log(currentData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
 }
