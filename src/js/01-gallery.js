bc74410e17629bc6fdb9c0604de93c71eac9aa30
NickKononenko <kononenko0812@gmail.com>
2023-08-11 11:46:21 +0300

    1st task completed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) => {
+    return (
+      acc +
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    </li>`
+    );
+  },
+  ''
+);
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.9,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
