d1af15292dcca994702d50909768fd970639cef7
NickKononenko <kononenko0812@gmail.com>
2023-08-11 17:57:41 +0300

    3rd commit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const submitBtn = document.querySelector('button');
+
+const userData = {
+  email: '',
+  message: '',
+};
+const savedUserData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+window.addEventListener('load', onLoad);
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+function onLoad() {
+  if (savedUserData) {
+    form.email.value = savedUserData.email;
+    form.message.value = savedUserData.message;
+  }
+}
+
+function onInput() {
+  userData.email = form.email.value;
+  userData.message = form.message.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(userData));
+}
+
+function onSubmit(event) {
+  if (event.target.nodeName === 'BUTTON') {
+    event.preventDefault();
+    localStorage.removeItem('feedback-form-state');
+    console.log(savedUserData);
+  }
+}
